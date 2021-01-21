08d42bfc40b7ef981337c4f3b036cdb5df13bb1c
Deion Nelson <>
2021-01-20 23:16:23 -0700

    This is my module's

---
+++ Module1/Module1/Module1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BakeryShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            int b = 3;
+            int c = 0;
+
+            b += 1;
+
+            c = a + b;
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
---
+++ Module3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Module2
+{
+    class Users
+    {
+        static void Main(string[] args)
+        {
+
+            string UsersName = "Name";
+
+            string result = UsersName.ToUpper();
+
+            Console.WriteLine(result);
+
+
+        }
+
+
+        class Roles
+        {
+
+        }
+
+        class Images
+        {
+
+        }
+
+        class Posts
+        {
+
+        }
+
+        class Comments
+        {
+
+        }
+
+    }
+
+}

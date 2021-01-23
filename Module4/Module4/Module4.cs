5217568b017afaab448027731ddbe1da07185cc5
Deion Nelson <>
2021-01-22 22:29:33 -0700

    Update

---
+++ Module4/Module4/Module4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Module4
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime myDateTimeVariable = new DateTime(2010, 05, 10);
+
+            DateTime myDateTimeVariable2 = new DateTime(1998, 01, 01);
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+            Console.WriteLine(result);
+                
+        }
+    }
+}
+
+
+
+ 
+
+
